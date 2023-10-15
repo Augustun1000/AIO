9a34ffa85a587d1a41391ad469c822e63830614d
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2023-10-15 15:06:18 -0300

    Update Instalar paquetes necesarios para openbox.sh
    Fix missing packet

--- Instalar paquetes necesarios para openbox.sh
+++ Instalar paquetes necesarios para openbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of packages to install
-packages=("sudo pacman -S xorg-server xorg-xinit xorg-xrandr openbox obconf pcmanfm pulseaudio pavucontrol geany opera htop neofetch picom xdg-user-dirs adapta-gtk-theme papirus-icon-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance nano xterm gimp rofi viewnior simplescreenrecorder")
+packages=("sudo pacman -S xorg-server xorg-xinit xorg-xrandr openbox obconf pcmanfm pulseaudio pavucontrol geany opera htop neofetch picom xdg-user-dirs adapta-gtk-theme papirus-icon-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance nano xterm gimp rofi viewnior")
 
 for package in "${packages[@]}"; do
    echo "Installing $package..."
