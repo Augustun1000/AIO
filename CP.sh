053b03028b039b35cfa78efd86943591289bb32e
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2023-10-16 17:18:58 -0300

    Delete CP.sh

--- CP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
-# List of packages to install
-packages=("gimp" "steam" "code" "flameshot" "baobab" "rofi" "xarchive" "viewnior" "meld" "mplayer" "papirus-icon-theme")
-
-for package in "${packages[@]}"; do
-   echo "Installing $package..."
-   sudo pacman -S --noconfirm "$package"
-done
