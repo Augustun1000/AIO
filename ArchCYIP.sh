9bbe7183411fc8041e679349903cbc24f3024aee
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2023-10-16 09:42:51 -0300

    Update ArchCYIP.sh
    Remove packages and fix command

--- ArchCYIP.sh
+++ ArchCYIP.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
+
 # List of packages to install
-packages=("simplescreenrecorder" "baobab" "visual-studio-code-bin" "rofi-screenshot")
+packages=("simplescreenrecorder")
 
 for package in "${packages[@]}"; do
    echo "Installing $package..."
-   yay -S --noconfirm "$package"
+   sudo yay -S --noconfirm "$package"
 done
+
+#"obmenu-generator"
