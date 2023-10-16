2d2f5b8544214df93a1bd459cf4bdb1f6f8772d6
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2023-10-16 17:14:42 -0300

    Update BPI.sh

--- BPI.sh
+++ BPI.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 # List of packages to install
-packages=("xorg-server" "xorg-xinit" "xorg-xrandr" "xdg-user-dirs" "openbox" "obconf" "arandr" "nitrogen" "pcmanfm" "pulseaudio" "pavucontrol" "geany" "opera" "htop" "neofetch" "picom" "adapta-gtk-theme" "tint2" "networkmanager" "xfce4-power-manager" "lm_sensors" "lxappearance-gtk3" "nano" "xterm" "p7zip")
-
-for package in "${packages[@]}"; do
-   echo "Installing $package..."
-   sudo pacman -S "$package"
-done
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xdg-user-dirs openbox obconf arandr nitrogen pcmanfm pulseaudio pavucontrol geany opera htop neofetch picom adapta-gtk-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance-gtk3 nano xterm rofi p7zip
