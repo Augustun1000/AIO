b60fd1c75977f29587db5dd0f22cccf0f0eadfdd
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2023-10-16 15:39:06 -0300

    Update BPI.sh

--- BPI.sh
+++ BPI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 # List of packages to install
-packages=("xorg-server" "xorg-xinit" "xorg-xrandr" "xdg-user-dirs" "openbox" "obconf" "pcmanfm" "pulseaudio" "pavucontrol" "code" "geany" "baobab" "opera" "htop" "neofetch" "picom" "adapta-gtk-theme" "papirus-icon-theme" "tint2" "networkmanager" "xarchive" "viewnior" "meld" "mplayer" "xfce4-power-manager" "lm_sensors" "lxappearance-gtk3" "nitrogen" "tint2" "arandr" "nano" "xterm" "gimp" "rofi" "steam" "p7zip" "flameshot")
+packages=("xorg-server" "xorg-xinit" "xorg-xrandr" "xdg-user-dirs" "openbox" "obconf" "pcmanfm" "pulseaudio" "pavucontrol" "geany" "baobab" "opera" "htop" "neofetch" "picom" "adapta-gtk-theme" "papirus-icon-theme" "tint2" "networkmanager" "xarchive" "viewnior" "meld" "mplayer" "xfce4-power-manager" "lm_sensors" "lxappearance-gtk3" "nitrogen" "tint2" "arandr" "nano" "xterm" "rofi" "p7zip")
 
 for package in "${packages[@]}"; do
    echo "Installing $package..."
