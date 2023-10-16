1311463a7ced84213552128fc98a0d4e4382285e
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2023-10-16 17:49:06 -0300

    Update BPI.sh
    Fix and optimize

--- BPI.sh
+++ BPI.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 # List of packages to install
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs openbox obconf arandr nitrogen pcmanfm pulseaudio pavucontrol geany opera htop neofetch picom adapta-gtk-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance-gtk3 nano xterm rofi p7zip xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo intel-compute-runtime gimp steam code flameshot baobab xarchive viewnior meld mplayer papirus-icon-theme
-
-cp -r /etc/xdg/openbox/  .config/
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs openbox obconf arandr nitrogen pcmanfm pulseaudio pavucontrol geany opera htop neofetch picom adapta-gtk-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance-gtk3 nano xterm rofi p7zip xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo intel-compute-runtime gimp steam code flameshot baobab xarchiver viewnior meld mplayer papirus-icon-theme
