daef6b066da360e71b69e7bec54763b10847c6cf
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-18 12:09:11 -0300

    Update PI.sh

--- PI.sh
+++ PI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
+sudo pacman -S --needed base-devel &&
+git clone https://aur.archlinux.org/paru.git &&
+cd paru &&
 makepkg -si
