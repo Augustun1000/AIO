787b77092c967c999489ad9e757191da4c7580bf
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-20 20:01:49 -0300

    Add files via upload

---
+++ Delete-All-Scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
+
+rm -rf Basic-Installer.sh &&
+rm -rf Update-Config.sh &&
+rm -rf Paru-Instaler.sh &&
+rm -rf Yay-Instaler.sh &&
+rm -rf QEMU-Instaler.sh &&
