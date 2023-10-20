93b87dea25f6c96a2445d315256e4c530db6241a
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-20 20:43:22 -0300

    Update Basic-Installer.sh

--- Basic-Installer.sh
+++ Basic-Installer.sh
@@ -7,8 +7,8 @@ chmod +x Yay-Installer.sh &&
 chmod +x QEMU-Installer.sh &&
 chmod +x Delete-All-Scripts.sh &&
 shopt -s dotglob
-m -rf /home/augustun1000/AIO/.git
 mv /home/augustun1000/AIO/* /home/augustun1000/
+m -rf /home/augustun1000/AIO
 
 # List of packages to install
 #sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs openbox obconf arandr nitrogen thunar pulseaudio pavucontrol htop neofetch picom adapta-gtk-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance-gtk3 vim nano xterm xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo intel-compute-runtime geany opera rofi p7zip gimp steam code kitty flameshot baobab xarchiver viewnior meld mplayer papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts
