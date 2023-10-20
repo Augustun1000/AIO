1027216683793a89183445a71f7e040541e56dff
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-20 19:57:52 -0300

    Delete MV.sh

--- MV.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
-
-chmod +x BPI.sh &&
-chmod +x CC.sh &&
-chmod +x PI.sh &&
-shopt -s dotglob
-rm -rf /home/augustun1000/AIO/.git
-mv /home/augustun1000/AIO/* /home/augustun1000/
