78cac555208030336ac70604a82d8a0fc111b415
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 20:29:47 -0300

    Update Install-Config.sh

--- Install-Config.sh
+++ Install-Config.sh
@@ -4,3 +4,9 @@
 xdg-user-dirs-update &&
 cp -r /etc/xdg/openbox/  .config/ &&
 7z -aoa /home/augustun1000/Downloads/.config.7z
+
+
+sudo pacman -S --needed base-devel &&
+git clone https://aur.archlinux.org/paru.git &&
+cd paru &&
+makepkg -si
