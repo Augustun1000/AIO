73a2913c8302aace8e8b1f4656b92272fe53d3bb
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 11:50:25 -0300

    Update Basic-Installer.sh

--- Basic-Installer.sh
+++ Basic-Installer.sh
@@ -16,10 +16,10 @@ mv /home/augustun1000/By-平沢Zen＠.png /home/augustun1000/Downloads &&
 mv /home/augustun1000/.config.7z /home/augustun1000/Downloads/ &&
 
 # List of packages to install
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa opencl-mesa lib32-opencl-mesa intel-media-driver libva-utils vdpauinfo clinfo openbox obconf arandr feh thunar pulseaudio pavucontrol htop neofetch adapta-gtk-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance-gtk3 nano geany opera rofi p7zip gimp steam code kitty flameshot baobab xarchiver viewnior meld mplayer papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts gnome-disk-utility gparted
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo openbox obconf arandr feh thunar pulseaudio pavucontrol htop neofetch adapta-gtk-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance-gtk3 nano geany opera rofi p7zip gimp steam code kitty flameshot baobab xarchiver viewnior meld mplayer papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts gnome-disk-utility gparted
 
 #Universal intel drivers (All gen 1 to up)
-#sudo pacman -S xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa opencl-mesa lib32-opencl-mesa
+#sudo pacman -S xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa
 
 #Drivers gen 8 to up
 #sudo pacman -S xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver intel-compute-runtime intel-media-driver
