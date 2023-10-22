b6b6c1453840b2d2d264cdd9b90f94bd86145abc
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 10:00:35 -0300

    Update Basic-Installer.sh

--- Basic-Installer.sh
+++ Basic-Installer.sh
@@ -6,13 +6,13 @@ chmod +x Paru-Installer.sh &&
 chmod +x Yay-Installer.sh &&
 chmod +x QEMU-Installer.sh &&
 chmod +x Delete-All-Scripts.sh &&
-shopt -s dotglob
-rm -rf /home/augustun1000/AIO/.git
-mv /home/augustun1000/AIO/* /home/augustun1000/
-rm -rf /home/augustun1000/AIO
-mv /home/augustun1000/Created-by-コダマ.png /home/augustun1000/Downloads
-mv /home/augustun1000/By-平沢Zen＠.png /home/augustun1000/Downloads
-mv /home/augustun1000/.config.7z /home/augustun1000/Downloads/
+shopt -s dotglob &&
+rm -rf /home/augustun1000/AIO/.git &&
+mv /home/augustun1000/AIO/* /home/augustun1000/ &&
+rm -rf /home/augustun1000/AIO &&
+mv /home/augustun1000/Created-by-コダマ.png /home/augustun1000/Downloads &&
+mv /home/augustun1000/By-平沢Zen＠.png /home/augustun1000/Downloads &&
+mv /home/augustun1000/.config.7z /home/augustun1000/Downloads/ &&
 
 # List of packages to install
 sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs openbox obconf arandr feh thunar pulseaudio pavucontrol htop neofetch adapta-gtk-theme tint2 networkmanager xfce4-power-manager lm_sensors lxappearance-gtk3 nano xterm xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo intel-compute-runtime geany opera rofi p7zip gimp steam code kitty flameshot baobab xarchiver viewnior meld mplayer papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts gnome-disk-utility gparted
