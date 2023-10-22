18dd098a537c199eb4461abe4defe81163073295
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 20:43:28 -0300

    Update Install-Config.sh

--- Install-Config.sh
+++ Install-Config.sh
@@ -5,9 +5,3 @@
 xdg-user-dirs-update &&
 cp -r /etc/xdg/openbox/  .config/ &&
 7z -aoa /home/augustun1000/Downloads/.config.7z
-
-#Install aur helper in this case Paru
-sudo pacman -S --needed base-devel &&
-git clone https://aur.archlinux.org/paru.git &&
-cd paru &&
-makepkg -si
