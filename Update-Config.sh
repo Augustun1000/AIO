b15720e36b1e2afd2a47c18b3aa5f0948cb9f4be
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 09:29:41 -0300

    Update Update-Config.sh

--- Update-Config.sh
+++ Update-Config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 
-xdg-user-dirs-update
-cp -r /etc/xdg/openbox/  .config/
+xdg-user-dirs-update &&
+cp -r /etc/xdg/openbox/  .config/ &&
+7z x /home/augustun1000/Downloads/.config.7z
