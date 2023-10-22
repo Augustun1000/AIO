52ef2bc66d2fc376a5fdc8a4963ba5acc5f83f2f
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 20:45:25 -0300

    Update Delete-All-Scripts.sh

--- Delete-All-Scripts.sh
+++ Delete-All-Scripts.sh
@@ -3,7 +3,7 @@
 
 rm -rf Basic-Installer.sh &&
 rm -rf Install-Config.sh &&
-rm -rf QEMU-Installer.sh &&
+rm -rf Vanities-Installer.sh &&
 rm -rf .config.zip &&
 rm -rf Virtualbox shared folder access.txt
 rm -rf Delete-All-Scripts.sh
