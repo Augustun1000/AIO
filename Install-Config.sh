2718614073849d3d59d3300c0db9a0f8154a8e5f
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 20:33:22 -0300

    Update Install-Config.sh

--- Install-Config.sh
+++ Install-Config.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 
+#My custom config for openbox, rofi, kitty terminal and tint2
 xdg-user-dirs-update &&
 cp -r /etc/xdg/openbox/  .config/ &&
 7z -aoa /home/augustun1000/Downloads/.config.7z
 
-
+#Install aur helper in this case Paru
 sudo pacman -S --needed base-devel &&
 git clone https://aur.archlinux.org/paru.git &&
 cd paru &&
