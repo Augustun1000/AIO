a0a9b134b19a9d780e0c75edd9f9314f78257add
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 20:42:16 -0300

    Update QEMU-Installer.sh

--- QEMU-Installer.sh
+++ QEMU-Installer.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 
-sudo pacman -S qemu-full
-
-# For graphic QEMU Virt-Manager OR GNOME Boxes(sudo pacman -S libvirt gnome-boxes)
-sudo pacman -S libvirt virt-manager
+# Install QEMU and graphic QEMU with Virt-Manager
+sudo pacman -S qemu-full libvirt virt-manager
 
 #For ip adress and ssh conection
 #sudo pacman -S iptables dnsmasq dmidecode bridge-utils openbsd-netcat
@@ -14,16 +12,3 @@ sudo pacman -S libvirt virt-manager
 
 #Temporal service
 #sudo systemctl start libvirtd.service
-
-
-#Linux firewall iptables-nft
-
-#// Si falla la conexión internet de NAT/DHCP
-#>see list
-#sudo virsh net-list --all
-#> NAT conection
-#sudo virsh net-define /etc/libvirt/qemu/networks/default.xml
-#> Automatic open with system
-#sudo virsh net-autostart default
-#> Active NAT
-#sudo virsh net-start default
