eb859c624057f76fe83dae6d9814731850cd8095
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2023-10-22 21:17:40 -0300

    Update Install-Config.sh

--- Install-Config.sh
+++ Install-Config.sh
@@ -4,4 +4,4 @@
 #My custom config for openbox, rofi, kitty terminal and tint2
 xdg-user-dirs-update &&
 cp -r /etc/xdg/openbox/  .config/ &&
-7z -aoa /home/augustun1000/Downloads/.config.7z
+7z -aoa /home/augustun1000/Desktop/.config.7z
