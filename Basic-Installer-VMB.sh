f35ce6adf4df8dd3e29ce5460a61950d61b522f5
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2024-02-29 19:07:19 -0300

    Update Basic-Installer-VMB.sh

--- Basic-Installer-VMB.sh
+++ Basic-Installer-VMB.sh
@@ -76,5 +76,5 @@ sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86
 #timedatectl set-time "2014-10-26 11:13:54"
 
 #VBoxManage showvminfo "idle"
-#VBoxManage setextradata idle "VBoxInternal/Devices/VMMDev/0/Config/GetHostTimeDisabled" 1
-#VBoxManage setextradata idle "VBoxInternal/TM/WarpDrivePercentage" 500
+#VBoxManage setextradata "idle" "VBoxInternal/Devices/VMMDev/0/Config/GetHostTimeDisabled" 1
+#VBoxManage setextradata "idle" "VBoxInternal/TM/WarpDrivePercentage" 500
