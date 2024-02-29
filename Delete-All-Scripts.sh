006c926f8cb1fc9ed5cbe239f1abac5adc7a68e3
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2024-02-29 15:50:10 -0300

    Update Delete-All-Scripts.sh

--- Delete-All-Scripts.sh
+++ Delete-All-Scripts.sh
@@ -2,12 +2,11 @@
 #Copyright (C) 2023 Augustun1000 <augustoperezriesgo@gmail.com>
 
 rm -rf Basic-Installer.sh &&
+rm -rf Basic-Installer-Netbook.sh &&
+rm -rf Basic-Installer-VMB.sh &&
+rm -rf .config.7z &&
 rm -rf Install-Config.sh &&
 rm -rf Vanities-Installer.sh &&
-rm -rf .config.7z &&
-rm -rf Basic-Installer-Netbook.sh &&
+rm -rf CyberSecurity.sh &&
 rm -rf README.md &&
 rm -rf Delete-All-Scripts.sh
-rm -rf Basic-Installer-VMB
-rm -rf CyberSecurity.sh
-rm -rf README.md
