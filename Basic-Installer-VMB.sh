f052872a8fc651c5fb74d2140cd2b4a323af9310
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2024-03-12 19:40:58 -0300

    Update Basic-Installer-VMB.sh

--- Basic-Installer-VMB.sh
+++ Basic-Installer-VMB.sh
@@ -10,7 +10,7 @@ rm -rf /home/augustun1000/AIO/.git &&
 mv /home/augustun1000/AIO/* /home/augustun1000/ &&
 rm -rf /home/augustun1000/AIO &&
 # List of packages to install
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo openbox obconf arandr feh thunar thunar-volman gvfs htop neofetch adapta-gtk-theme tint2 networkmanager lm_sensors lxappearance-gtk3 nano geany p7zip rofi steam kitty xterm papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk brightnessctl mtpfs gvfs-mtp gvfs-gphoto2 gnome-disk-utility kronometer
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo openbox obconf arandr feh thunar thunar-volman gvfs adapta-gtk-theme tint2 networkmanager lm_sensors lxappearance-gtk3 nano geany p7zip rofi steam kitty xterm papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk brightnessctl mtpfs gvfs-mtp gvfs-gphoto2 gnome-disk-utility kronometer
 
 #Universal intel drivers (All gen 1 to up)
 #sudo pacman -S xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa
