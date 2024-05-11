838dc9bbe11cfb3d46856a5fc44168b1981e58dc
Augusto Perez Riesgo <104395373+Augustun1000@users.noreply.github.com>
2024-05-11 00:02:36 -0300

    Update Install-Config.sh

--- Install-Config.sh
+++ Install-Config.sh
@@ -5,6 +5,7 @@
 xdg-user-dirs-update &&
 cp -r /etc/xdg/openbox/  .config/ &&
 7z x .config.7z -aoa &&
+7z x .local.7z -aoa &&
 mv /home/augustun1000/Created-by-コダマ.png /home/augustun1000/Downloads &&
 mv /home/augustun1000/By-平沢Zen＠.png /home/augustun1000/Downloads &&
 startx
