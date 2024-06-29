8809456a8876330c6010b6f4d4c9672eb5fb5935
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2024-06-29 10:43:03 -0300

    Update Basic-Installer.sh

--- Basic-Installer.sh
+++ Basic-Installer.sh
@@ -10,7 +10,7 @@ rm -rf /home/augustun1000/AIO/.git &&
 mv /home/augustun1000/AIO/* /home/augustun1000/ &&
 rm -rf /home/augustun1000/AIO &&
 # List of packages to install
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo openbox obconf yad arandr feh thunar thunar-volman gvfs htop neofetch adapta-gtk-theme tint2 networkmanager lm_sensors lxappearance-gtk3 nano geany rofi p7zip gimp steam code kitty xterm flameshot baobab xarchiver viewnior meld mplayer papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk gnome-disk-utility gparted pulseaudio pulsemixer brightnessctl mtpfs gvfs-mtp gvfs-gphoto2 xfce4-power-manager
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa intel-media-driver libva-utils vdpauinfo clinfo openbox obconf yad arandr feh thunar thunar-volman gvfs htop neofetch adapta-gtk-theme tint2 networkmanager lm_sensors lxappearance-gtk3 nano geany rofi p7zip gimp steam code kitty xterm flameshot baobab xarchiver viewnior meld mplayer papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk gnome-disk-utility gparted pulseaudio pulsemixer mtpfs gvfs-mtp gvfs-gphoto2 xfce4-power-manager
 
 
 #Universal intel drivers (All gen 1 to up)
